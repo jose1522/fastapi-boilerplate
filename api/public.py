272340b77e70d0d43dfd0919522f09c51d2746da
Jose Andres Moya Porras <jose.91cr@gmail.com>
2020-10-12 00:19:29 -0600

    Adding token endpoint

--- api/public.py
+++ api/public.py
@@ -1,9 +1,20 @@
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Path, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from core.security.authentication import credentials_exception
 from api.validation import *
 from database import model
 from typing import Optional
 
 public = APIRouter()
+
+
+@public.post('/token', response_model=TokenParams)
+async def token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = UserParams(**form_data.__dict__)
+    result = await model.User.authenticate(user)
+    if not result.get('Token'):
+        raise  credentials_exception
+    return {"access_token": result.get("Token"), "token_type": "bearer"}
 
 
 @public.get('/{name}')
